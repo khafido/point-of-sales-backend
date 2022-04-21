9acc8a31b819e2cce58d368a321d1435baeb8f17
MITRAIS\Khafido_I343 <khafido.work@gmail.com>
2022-04-21 14:43:57 +0700

    spring init

---
+++ src/main/java/com/mitrais/cdcpos/CdcPosApplication.java
@@ -0,0 +1,13 @@
+package com.mitrais.cdcpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitrais/cdcpos/CdcPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitrais.cdcpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
