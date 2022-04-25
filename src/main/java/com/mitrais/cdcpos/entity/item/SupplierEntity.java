2087d5aa67f3450c3ef5292a19e52f7f39d7ec52
Pradipta RK <pradiptark@gmail.com>
2022-04-25 15:34:15 +0700

    added supplier entity and repo

--- src/main/java/com/mitrais/cdcpos/entity/item/SupplierEntity.java
+++ src/main/java/com/mitrais/cdcpos/entity/item/SupplierEntity.java
@@ -1,2 +1,39 @@
-package com.mitrais.cdcpos.entity.item;public class supplier {
+package com.mitrais.cdcpos.entity.item;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "suppliers")
+public class SupplierEntity {
+    @Id
+    private UUID transferId = UUID.randomUUID();
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cp_name")
+    private String CPName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
 }
--- src/main/java/com/mitrais/cdcpos/repository/SupplierRepository.java
+++ src/main/java/com/mitrais/cdcpos/repository/SupplierRepository.java
@@ -1,2 +1,12 @@
-package com.mitrais.cdcpos.repository;public interface SupplierRepository {
+package com.mitrais.cdcpos.repository;
+
+import com.mitrais.cdcpos.entity.item.SupplierEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<SupplierEntity, UUID> {
 }
