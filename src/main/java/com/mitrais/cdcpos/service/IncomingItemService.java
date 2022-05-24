4e78fed65a91f63e8e4e42d5c1bb0d94d36c4ab1
MITRAIS\Khafido_I343 <khafido.work@gmail.com>
2022-05-24 13:56:39 +0700

    add Math Context when divide BigDecimal

--- src/main/java/com/mitrais/cdcpos/service/IncomingItemService.java
+++ src/main/java/com/mitrais/cdcpos/service/IncomingItemService.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,7 +40,8 @@ public class IncomingItemService {
         incomingItem.setSupplier(supplier);
         incomingItem.setBuyQty(req.getQty());
         incomingItem.setBuyPrice(req.getBuyPrice());
-        incomingItem.setPricePerItem(req.getBuyPrice().divide(BigDecimal.valueOf(req.getQty())));
+        MathContext mc = new MathContext(String.valueOf(req.getBuyPrice()).length()-1);
+        incomingItem.setPricePerItem(req.getBuyPrice().divide(BigDecimal.valueOf(req.getQty()), mc));
         incomingItem.setBuyDate(req.getBuyDate());
         incomingItem.setExpiryDate(req.getExpiryDate());
 
