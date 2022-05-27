043194d543b41a638ba6d923b52045d704039926
Alvian Nur Ramadhani <alviannur.ramadhani@mitrais.com>
2022-05-27 07:23:44 +0700

    unit test for get all incomingitem and get all expired

--- src/test/java/com/mitrais/cdcpos/service/IncomingItemServiceTest.java
+++ src/test/java/com/mitrais/cdcpos/service/IncomingItemServiceTest.java
@@ -2,8 +2,11 @@ package com.mitrais.cdcpos.service;
 
 import com.mitrais.cdcpos.dto.IncomingItemDto;
 import com.mitrais.cdcpos.dto.IncomingItemResponseDto;
+import com.mitrais.cdcpos.entity.CategoryEntity;
 import com.mitrais.cdcpos.entity.item.IncomingItemEntity;
+import com.mitrais.cdcpos.entity.item.ItemEntity;
 import com.mitrais.cdcpos.entity.item.SupplierEntity;
+import com.mitrais.cdcpos.entity.store.StoreEntity;
 import com.mitrais.cdcpos.entity.store.StoreItemEntity;
 import com.mitrais.cdcpos.repository.IncomingItemRepository;
 import com.mitrais.cdcpos.repository.StoreItemRepository;
@@ -16,19 +19,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.awt.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -45,51 +48,86 @@ public class IncomingItemServiceTest {
     @InjectMocks
     private IncomingItemService incomingItemService;
     
-    private List<IncomingItemEntity> listItem;
+    private final List<StoreEntity> storeList = new ArrayList<>();
+    private final List<ItemEntity> itemList = new ArrayList<>();
+    private final List<CategoryEntity> categoryList = new ArrayList<>();
+    private final List<StoreItemEntity> storeItemList = new ArrayList<>();
+    private final List<IncomingItemEntity> incomingItemList = new ArrayList<>();
+    private final List<SupplierEntity> supplierList = new ArrayList<>();
+
     @BeforeEach
     public void init(){
-        IncomingItemEntity item1 = mock(IncomingItemEntity.class);
-        IncomingItemEntity item2 = mock(IncomingItemEntity.class);
-        IncomingItemEntity item3 = mock(IncomingItemEntity.class);
-        listItem = new ArrayList<>(Arrays.asList(item1,item2,item3));
+        supplierList.add(new SupplierEntity(UUID.randomUUID(),"sup 1", "sup 1", "0812138132","sup@gmail.com",
+                "jalan raya"));
+        categoryList.add(new CategoryEntity(UUID.randomUUID(), "makanan"));
+        categoryList.add(new CategoryEntity(UUID.randomUUID(), "minuman"));
+        storeList.add(new StoreEntity(UUID.randomUUID(),"store", "yogya", null,null,null));
+
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setName("Makanan 1");
+        itemEntity.setImage("image");
+        itemEntity.setBarcode("091212");
+        itemEntity.setCategory(categoryList.get(0));
+        itemEntity.setPackaging("wrap");
+        itemList.add(itemEntity);
+
+        StoreItemEntity storeItem = new StoreItemEntity();
+        storeItem.setStore(storeList.get(0));
+        storeItem.setItem(itemList.get(0));
+        storeItem.setStock(20);
+        storeItem.setFixedPrice(new BigDecimal(20000));
+        storeItem.setPriceMode(StoreItemEntity.PriceMode.BY_SYSTEM);
+        storeItemList.add(storeItem);
+
+        IncomingItemEntity incomingItem = new IncomingItemEntity();
+        incomingItem.setSupplier(supplierList.get(0));
+        incomingItem.setBuyQty(120L);
+        incomingItem.setBuyPrice(new BigDecimal(1200000));
+        incomingItem.setStoreItem(storeItemList.get(0));
+        incomingItem.setPricePerItem(new BigDecimal(40000));
+        incomingItem.setBuyDate(LocalDateTime.now());
+        incomingItem.setExpiryDate(LocalDate.now().plusYears(3));
+        incomingItemList.add(incomingItem);
     }
 
     @Test
     public void addIncomingItem(){
-        var storeItem = mock(StoreItemEntity.class);
-        var supplier = mock(SupplierEntity.class);
+        when(storeItemRepository.findById(storeItemList.get(0).getId())).thenReturn(Optional.of(storeItemList.get(0)));
+        when(supplierRepository.findByIdAndDeletedAtIsNull(supplierList.get(0).getId())).thenReturn(Optional.of(supplierList.get(0)));
+        when(storeItemRepository.save(any(StoreItemEntity.class))).thenReturn(storeItemList.get(0));
+        when(incomingItemRepository.save(any(IncomingItemEntity.class))).thenReturn(incomingItemList.get(0));
 
-        when(storeItemRepository.findById(storeItem.getId())).thenReturn(Optional.of(storeItem));
-        when(supplierRepository.findByIdAndDeletedAtIsNull(supplier.getId())).thenReturn(Optional.of(supplier));
-        when(storeItemRepository.save(any(StoreItemEntity.class))).thenReturn(storeItem);
-        when(incomingItemRepository.save(any(IncomingItemEntity.class))).thenReturn(listItem.get(0));
-
-        var incomingDto = new IncomingItemDto(storeItem.getId(),supplier.getId(),new BigDecimal(12000)
+        var incomingDto = new IncomingItemDto(storeItemList.get(0).getId(),supplierList.get(0).getId(),new BigDecimal(12000)
                 ,100,LocalDateTime.now(), LocalDate.now().plusYears(5));
         var result = incomingItemService.add(incomingDto);
-        assertEquals(listItem.get(0),result);
+        assertEquals(incomingItemList.get(0),result);
 
-        verify(storeItemRepository).findById(storeItem.getId());
-        verify(supplierRepository).findByIdAndDeletedAtIsNull(supplier.getId());
+        verify(storeItemRepository).findById(storeItemList.get(0).getId());
+        verify(supplierRepository).findByIdAndDeletedAtIsNull(supplierList.get(0).getId());
         verify(storeItemRepository).save(any(StoreItemEntity.class));
         verify(incomingItemRepository).save(any(IncomingItemEntity.class));
 
     }
     
-//    @Test
-//    public void getIncomingItemNotPaged(){
-//        when(incomingItemRepository.findAllSearch((Sort) any(),anyString(),
-//                any(), any())).thenReturn(listItem);
-//
-//        Page<IncomingItemResponseDto> result = incomingItemService.getAll(
-//                false,0,3,"","storeItem.item.name", "asc", LocalDateTime.now().minusYears(10),LocalDateTime.now());
-//        List<IncomingItemResponseDto> items = listItem.stream().map(IncomingItemResponseDto::toDto).collect(Collectors.toList());
-//        for(int i=0; i<=2;i++){
-//            assertSame(items.get(i),result.getContent().get(i).getBuyDate());
-//        }
-//
-//        verify(incomingItemRepository).findAllSearch((Sort) any(),anyString(),
-//                LocalDateTime.now().minusYears(10), LocalDateTime.now());
-//    }
-    
+
+    @Test
+    public void getIncomingItem(){
+        when(incomingItemRepository.findAllSearch((Pageable) any(), anyString(), any(),any()))
+                .thenReturn(new PageImpl<>(incomingItemList));
+        when(incomingItemRepository.findAllSearch((Sort) any(), anyString(),any(), any()))
+                .thenReturn(incomingItemList);
+
+        Page<IncomingItemResponseDto> result = incomingItemService.getAll(
+                false,0,3,"","storeItem.item.name", "asc", LocalDateTime.now().minusYears(10), LocalDateTime.now());
+        Page<IncomingItemResponseDto> resultPaged = incomingItemService.getAll(
+                true,0,3,"","storeItem.item.name", "asc", LocalDateTime.now().minusYears(10), LocalDateTime.now());
+
+
+        List<IncomingItemResponseDto> items = incomingItemList.stream().map(IncomingItemResponseDto::toDto).collect(Collectors.toList());
+        assertTrue(resultPaged.equals(result));
+        assertEquals(items.get(0), result.getContent().get(0));
+
+        verify(incomingItemRepository).findAllSearch((Pageable) any(), anyString(), any(),any());
+        verify(incomingItemRepository).findAllSearch((Sort) any(), anyString(),any(), any());
+    }
 }
--- src/test/java/com/mitrais/cdcpos/service/StoreServiceTest.java
+++ src/test/java/com/mitrais/cdcpos/service/StoreServiceTest.java
@@ -1,11 +1,18 @@
 package com.mitrais.cdcpos.service;
 
+import com.mitrais.cdcpos.dto.IncomingItemResponseDto;
 import com.mitrais.cdcpos.dto.store.StoreAssignManagerRequestDto;
 import com.mitrais.cdcpos.dto.store.StoreDto;
+import com.mitrais.cdcpos.entity.CategoryEntity;
 import com.mitrais.cdcpos.entity.auth.ERole;
 import com.mitrais.cdcpos.entity.auth.RoleEntity;
 import com.mitrais.cdcpos.entity.auth.UserEntity;
+import com.mitrais.cdcpos.entity.item.IncomingItemEntity;
+import com.mitrais.cdcpos.entity.item.ItemEntity;
+import com.mitrais.cdcpos.entity.item.SupplierEntity;
 import com.mitrais.cdcpos.entity.store.StoreEntity;
+import com.mitrais.cdcpos.entity.store.StoreItemEntity;
+import com.mitrais.cdcpos.repository.IncomingItemRepository;
 import com.mitrais.cdcpos.repository.StoreRepository;
 import com.mitrais.cdcpos.repository.UserRepository;
 import lombok.SneakyThrows;
@@ -15,12 +22,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -33,10 +44,18 @@ class StoreServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    IncomingItemRepository incomingItemRepository;
+
     @InjectMocks
     private StoreService storeService;
 
     private static final List<StoreEntity> storeList = new ArrayList<>();
+    private static final List<ItemEntity> itemList = new ArrayList<>();
+    private static final List<CategoryEntity> categoryList = new ArrayList<>();
+    private static final List<StoreItemEntity> storeItemList = new ArrayList<>();
+    private static final List<IncomingItemEntity> incomingItemList = new ArrayList<>();
+    private static final List<SupplierEntity> supplierList = new ArrayList<>();
 
     @BeforeAll
     static void beforeAll() {
@@ -47,6 +66,39 @@ class StoreServiceTest {
             storeEntity.setManager(null);
             storeList.add(storeEntity);
         }
+
+        /*
+        * init for Incoming item
+        * */
+        supplierList.add(new SupplierEntity(UUID.randomUUID(),"sup 1", "sup 1", "0812138132","sup@gmail.com",
+                "jalan raya"));
+        categoryList.add(new CategoryEntity(UUID.randomUUID(), "makanan"));
+        categoryList.add(new CategoryEntity(UUID.randomUUID(), "minuman"));
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setName("Makanan 1");
+        itemEntity.setImage("image");
+        itemEntity.setBarcode("091212");
+        itemEntity.setCategory(categoryList.get(0));
+        itemEntity.setPackaging("wrap");
+        itemList.add(itemEntity);
+
+        StoreItemEntity storeItem = new StoreItemEntity();
+        storeItem.setStore(storeList.get(0));
+        storeItem.setItem(itemList.get(0));
+        storeItem.setStock(20);
+        storeItem.setFixedPrice(new BigDecimal(20000));
+        storeItem.setPriceMode(StoreItemEntity.PriceMode.BY_SYSTEM);
+        storeItemList.add(storeItem);
+
+        IncomingItemEntity incomingItem = new IncomingItemEntity();
+        incomingItem.setSupplier(supplierList.get(0));
+        incomingItem.setBuyQty(120L);
+        incomingItem.setBuyPrice(new BigDecimal(1200000));
+        incomingItem.setStoreItem(storeItemList.get(0));
+        incomingItem.setPricePerItem(new BigDecimal(40000));
+        incomingItem.setBuyDate(LocalDateTime.now());
+        incomingItem.setExpiryDate(LocalDate.now().plusYears(3));
+        incomingItemList.add(incomingItem);
     }
 
     @Test
@@ -186,6 +238,24 @@ class StoreServiceTest {
         verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(user.getId());
         verify(storeRepository, times(1)).findByIdEqualsAndDeletedAtIsNull(store.getId());
         verify(storeRepository, times(1)).save(store);
+    }
+
+    @Test
+    public void getAllExpiredStoreItem(){
+        when(incomingItemRepository.findAllExpired((Pageable) any(),  any(), anyString(),any(),any())).thenReturn(new PageImpl<>(incomingItemList));
+        when(incomingItemRepository.findAllExpired((Sort) any(),  any(), anyString(),any(),any())).thenReturn(incomingItemList);
+
+        Page<IncomingItemResponseDto> result = storeService.storeListOfExpiredItems(storeList.get(0).getId(),false,0,3,"",
+                "supplier","asc",LocalDateTime.now().minusYears(10),LocalDateTime.now());
+        Page<IncomingItemResponseDto> resultPaged = storeService.storeListOfExpiredItems(storeList.get(0).getId(),true,0,3,"",
+                "supplier","asc",LocalDateTime.now().minusYears(10),LocalDateTime.now());
+
+        List<IncomingItemResponseDto> items = incomingItemList.stream().map(IncomingItemResponseDto::toDto).collect(Collectors.toList());
+        assertTrue(resultPaged.equals(result));
+        assertEquals(items.get(0), result.getContent().get(0));
+
+        verify(incomingItemRepository).findAllExpired((Pageable) any(),  any(), anyString(),any(),any());
+        verify(incomingItemRepository).findAllExpired((Sort) any(),  any(), anyString(),any(),any());
     }
 }
 
